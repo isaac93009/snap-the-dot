595313919dd43238faabaa29af3f959af9009b7f
isaac93009 <133547381+isaac93009@users.noreply.github.com>
2023-05-13 20:15:45 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    if (sprite.get(LedSpriteProperty.X) == 2) {
+        game.addScore(1)
+    } else {
+        game.gameOver()
+    }
+})
+let sprite: game.LedSprite = null
+music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
+sprite = game.createSprite(2, 2)
+basic.forever(function () {
+    sprite.move(1)
+    sprite.ifOnEdgeBounce()
+    basic.pause(200)
+})
